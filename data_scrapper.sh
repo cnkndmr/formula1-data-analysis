23c90af54b98476d4aa54f63a9d97ea8cbadb22a
M. Can Kandemir <cnkndmr@gmail.com>
2020-08-05 02:52:19 +0300

    numbers printing as two digits

--- data_scrapper.sh
+++ data_scrapper.sh
@@ -3,9 +3,8 @@
 # Email: cnkndmr@gmail.com
 
 lap_info () {
-	mkdir -p "data/year_$1/race_$2_$4"
-	curl "https://ergast.com/api/f1/$1/$2/laps/$3.json" | sed 's/{"driverId"/"\n{"driverId"/g; s/[{}]//g; s/\[//g; s/\]//g' | grep "\"driverId\"\:" | sed  's/"driverId"\://g; s/"position"\://g; s/"time"\://g; s/,"$//g' > "data/year_$1/race_$2_$4/lap_$3.csv"
-	
+	mkdir -p "data/year_$1/race_$(printf %02d "$2")_$4"
+	curl "https://ergast.com/api/f1/$1/$2/laps/$3.json" | sed 's/{"driverId"/"\n{"driverId"/g; s/[{}]//g; s/\[//g; s/\]//g' | grep "\"driverId\"\:" | sed  's/"driverId"\://g; s/"position"\://g; s/"time"\://g; s/,"$//g' > "data/year_$1/race_$(printf %02d "$2")_$4/lap_$(printf %02d "$3").csv"
 }
 
 for i in {2019..2020}
@@ -16,17 +15,17 @@ do
 		for k in {1..100}
 		do
 			lap_info "$i" "$j" "$k" "$raceId"
-			if [ "$(du data/year_"$i"/race_"$j"_"$raceId"/lap_"$k".csv | cut -f 1)" -eq "0" ]
+			if [ "$(du data/year_"$i"/race_"$(printf %02d "$j")"_"$raceId"/lap_"$(printf %02d "$k")".csv | cut -f 1)" -eq "0" ]
 			then
 				echo "Empty lap code break"
-				rm data/year_"$i"/race_"$j"_"$raceId"/lap_"$k".csv
+				rm data/year_"$i"/race_"$(printf %02d "$j")"_"$raceId"/lap_"$(printf %02d "$k")".csv
 				break 1
 			fi
 		done
-		if [ "$(ls data/year_"$i"/race_"$j"_"$raceId" | wc -l)" -eq "0" ]
+		if [ "$(ls data/year_"$i"/race_"$(printf %02d "$j")"_"$raceId" | wc -l)" -eq "0" ]
 			then
 				echo "Empty race code break"
-				rmdir data/year_"$i"/race_"$j"_"$raceId"
+				rmdir data/year_"$i"/race_"$(printf %02d "$j")"_"$raceId"
 				break 1
 			fi
 	done
